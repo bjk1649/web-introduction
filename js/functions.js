4d7213a77f228a766196c0149cd4e31add9f4b2a
bjk1649 <qhwns1649@naver.com>
2023-02-16 15:20:44 +0900

    feat: 각 버튼에 기능 추가

---
+++ js/functions.js
@@ -0,0 +1,41 @@
+var enter = document.getElementById('enter');
+enter.addEventListener('click', enterVisitorBook);
+
+function enterVisitorBook() {
+    const comments = document.getElementById("comments");
+    const newComment = document.createElement("li");
+    const commentText = document.getElementById("commentText");
+    const name = commentText.value;
+    commentText.value = commentText.value + "가 좋아하는 영화 목록 ";
+    let overlap = 0;
+    commentText.value = commentText.value + ": "
+    let overlapMovieName = []; 
+
+    console.log('commentTextClear');
+    var movie_length = document.getElementsByName("movie").length;
+    for (var i=0; i<movie_length; i++) {
+        if (document.getElementsByName("movie")[i].checked == true) {
+            document.getElementsByName("movie")[i].checked = false;
+            overlapMovieName.push(document.getElementsByName("movie")[i].value);
+            overlap++;
+        }
+    }
+    commentText.value = commentText.value + overlapMovieName.join(", ") + "\n";
+
+
+    const comment = document.createTextNode(commentText.value);
+    newComment.appendChild(comment);
+    comments.appendChild(newComment);
+    commentText.value = "";
+    alert(name + "님, 저와 " + overlap + "개의 취향이 같으시네요!");
+}
+
+function clearComment() {
+    const comments = document.getElementById("comments");
+    const contents = comments.get
+  
+    for (let i = 0; i < contents.length; i++) {
+      contents.item[i].remove();
+    }
+    
+}
